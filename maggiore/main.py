c1cd43596ac78d3af77c1f22c8fa93674dcc363e
Fra <162883721+Fiverook98@users.noreply.github.com>
2025-05-31 18:34:29 +0200

    Delete maggiore directory

--- maggiore/main.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Inserisci il primo numero: "))
-b = int(input("Inserisci il secondo numero: "))
-
-if a > b:
-    m = a
-else:
-    m = b
-
-print("Il numero maggiore tra ", a, "e", b, "è", m)
